b1c24c5d90be5bec61c193d24702022fcddb9b78
Tilda Lu <tilda.lu@trunk-studio.com>
2021-02-01 15:01:42 +0800

    Initialize project using Create React App

---
+++ src/config/config.js
@@ -0,0 +1,36 @@
+import { lazy } from 'react'
+import locales from './locales'
+import routes from './routes'
+import getMenuItems from './menuItems'
+import themes from './themes'
+import parseLanguages from 'base-shell/lib/utils/locale'
+
+const config = {
+  auth: {
+    signInURL: '/signin',
+  },
+  routes,
+  locale: {
+    locales,
+    defaultLocale: parseLanguages(['en', 'de', 'ru'], 'en'),
+    onError: (e) => {
+      //console.warn(e)
+      return 
+    },
+  },
+  menu: {
+    getMenuItems,
+  },
+  theme: {
+    themes,
+    defaultThemeID: 'default',
+    defaultIsDarkMode: false,
+    defaultIsRTL: false //change this to true for Right to Left Language support
+  },
+  pages: {
+    LandingPage: lazy(() => import('../pages/LandingPage/LandingPage')),
+    PageNotFound: lazy(() => import('../pages/PageNotFound/PageNotFound')),
+  },
+}
+
+export default config
