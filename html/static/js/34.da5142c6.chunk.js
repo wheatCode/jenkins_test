f68ddb6a0d694089130a02c116deba11b03e32bc
蔡名彥 <a976543257@gmail.com>
2021-03-27 21:57:22 +0800

    add locations Table  zip field

--- html/static/js/34.da5142c6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[34],{952:function(e,t,n){"use strict";n.r(t);n(0);var i=n(87),s=n(7);t.default=function(){return Object(s.jsxs)("div",{style:{position:"absolute",left:0,top:0,bottom:0,right:0,display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"},children:[Object(s.jsx)("h2",{children:"Landing Page"}),Object(s.jsx)("div",{children:Object(s.jsx)(i.Link,{to:"/home",children:"Home"})})]})}}}]);
-//# sourceMappingURL=34.da5142c6.chunk.js.map
